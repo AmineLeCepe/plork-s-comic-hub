e0898d2cc040d65924915cc60bd69beac0807cca
AmineLeCepe <lionannaba@gmail.com>
2025-07-04 18:56:29 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../frontend/views'));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '../frontend/public')));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
