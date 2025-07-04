6c976e4da7d89e10b77c9daca8bddaa92189c538
AmineLeCepe <lionannaba@gmail.com>
2025-07-04 21:35:27 +0100

    Second commit

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.set('views', path.join(__dirname, '../frontend/views'));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on port ${process.env.PORT || 3000}`);
 })
 
 app.get("/", (req, res) => {
---
+++ todo.md
@@ -0,0 +1,22 @@
+# Classes
+## Comic
+Name
+Description
+Language
+Additional links
+Authors
+Image
+Tags
+Date
+View count(?)
+Favorites(?)
+Comments(?)
+## Chapter
+Title
+Number
+Pages
+Cover
+Date
+View count(?)
+Favorites(?)
+Comments(?)
