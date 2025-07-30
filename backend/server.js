ea13a1fdab0f96f57f79a46cb0b1ff8e5cfd4af0
AmineLeCepe <lionannaba@gmail.com>
2025-07-30 21:27:42 +0100

    Basic login debugging

--- backend/server.js
+++ backend/server.js
@@ -93,3 +93,10 @@ app.post("/register", async (req, res) => {
         });
     }
 })
+
+app.post("/login", async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+    res.redirect("/profile");
+
+})
